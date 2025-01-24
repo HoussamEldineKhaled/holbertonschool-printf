e614292f703bb9e4c30e5b983cd70f73234c2038
root <houssam.khaled@lau.edu>
2025-01-24 21:47:34 +0000

    test2

--- _printf.c
+++ _printf.c
@@ -1,45 +1,55 @@
 #include "main.h"
 #include <stdarg.h>
 #include <unistd.h>
+
 int _printf(const char *format, ...)
 {
+  int i = 0;
   int count = 0;
   char c;
   char *s;
   va_list args;
 
   va_start(args, format);
-  while(*format != '\0')
+  while(format && format[i])
     {
-      if (*format == '%')
+      if (format[i] == '%')
 	{
-	  format++;
-	  if (*format == '%')
-	    {
-	      write(1, "%", 1);
-	      format++;
-	      count++;
-	    }
-	  else if (*format == 'c')
+	  i++;
+	  if (format[i] == 'c')
 	    {
 	      c = (char) va_arg(args, int);
 	      write(1, &c, 1);
-	      format++;
 	      count++;
 	    }
-	  else if(*format == 's')
+	  else if (format[i] == 's')
 	    {
 	      s = va_arg(args, char *);
 	      while (*s)
-		{	
-	      write(1, s, 1);
-	      s++;
+		{
+		  write(1, s, 1);
+		  count++;
+		  s++;
+	        }
+	    }
+	  else if (format[i] == '%')
+	    {
+	      write(1, "%", 1);
 	      count++;
-		}
-	      format++;
+	    }
+	  else
+	    {
+	      write(1, "%", 1);
+	      write(1, &format[i], 1);
+	      count += 2;
 	    }
 	}
-
+      else
+	{
+	  write(1, &format[i], 1);
+	  count++;
+	}
+      i++;
     }
   va_end(args);
       return (count);
