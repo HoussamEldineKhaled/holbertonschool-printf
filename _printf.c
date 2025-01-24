9552047a15b4c33d4b95d663d8475f47aa67e49f
root <houssam.khaled@lau.edu>
2025-01-24 21:57:54 +0000

    test3

--- _printf.c
+++ _printf.c
@@ -1,65 +1,67 @@
 #include "main.h"
 #include <stdarg.h>
 #include <unistd.h>
-
+/**
+ * _printf - printing s, c, and %
+ * @format: string format
+ * Return: -1 or count
+*/
 int _printf(const char *format, ...)
 {
-  int i = 0;
-  int count = 0;
-  char c;
-  char *s;
-  va_list args;
-
-  va_start(args, format);
-
-  if (!format)
-    {
-      return (-1);
-    }
-  while(format && format[i])
-    {
-      if (format[i] == '%')
-	{
-	  i++;
-	  if (format[i] == 'c')
-	    {
-	      c = (char) va_arg(args, int);
-	      write(1, &c, 1);
-	      count++;
-	    }
-	  else if (format[i] == 's')
-	    {
-	      s = va_arg(args, char *);
-	      if (!s)
-		{
-		  s = "(null)";
-		}
-	      while (*s)
-		{
-		  write(1, s, 1);
-		  count++;
-		  s++;
-	        }
-	    }
-	  else if (format[i] == '%')
-	    {
-	      write(1, "%", 1);
-	      count++;
-	    }
-	  else
-	    {
-	      write(1, "%", 1);
-	      write(1, &format[i], 1);
-	      count += 2;
-	    }
-	}
-      else
-	{
-	  write(1, &format[i], 1);
-	  count++;
-	}
-      i++;
-    }
-  va_end(args);
-      return (count);
+int i = 0;
+int count = 0;
+char c;
+char *s;
+va_list args;
+va_start(args, format);
+if (!format)
+{
+return (-1);
+}
+while(format && format[i])
+{
+if (format[i] == '%')
+{
+i++;
+if (format[i] == 'c')
+{
+c = (char) va_arg(args, int);
+write(1, &c, 1);
+count++;
+}
+else if (format[i] == 's')
+{
+s = va_arg(args, char *);
+if (!s)
+{
+s = "(null)";
+}
+while (*s)
+{
+write(1, s, 1);
+count++;
+s++;
+}
+}
+else if (format[i] == '%')
+{
+write(1, "%", 1);
+count++;
+}
+else
+{
+write(1, "%", 1);
+write(1, &format[i], 1);
+count += 2;
+}
+}
+else
+{
+write(1, &format[i], 1);
+count++;
+}
+i++;
+}
+va_end(args);
+return (count);
 }
