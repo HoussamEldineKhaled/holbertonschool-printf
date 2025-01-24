ca6cd681d45e7a2b63cb9f9e4a1cc20b2f88218f
root <houssam.khaled@lau.edu>
2025-01-24 20:49:17 +0000

    test

--- _printf.c
+++ _printf.c
@@ -1,13 +1,46 @@
 #include "main.h"
-
+#include <stdarg.h>
+#include <unistd.h>
 int _printf(const char *format, ...)
 {
-  int i = 0;
+  int count = 0;
+  char c;
+  char *s;
   va_list args;
 
   va_start(args, format);
+  while(*format != '\0')
+    {
+      if (*format == '%')
+	{
+	  format++;
+	  if (*format == '%')
+	    {
+	      write(1, "%", 1);
+	      format++;
+	      count++;
+	    }
+	  else if (*format == 'c')
+	    {
+	      c = (char) va_arg(args, int);
+	      write(1, &c, 1);
+	      format++;
+	      count++;
+	    }
+	  else if(*format == 's')
+	    {
+	      s = va_arg(args, char *);
+	      while (*s)
+		{	
+	      write(1, s, 1);
+	      s++;
+	      count++;
+		}
+	      format++;
+	    }
+	}
 
-
-
+    }
   va_end(args);
+      return (count);
 }
